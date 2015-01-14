76fbbfa88cd570f5ad9b2135f1fa2c05ed8d2e28
Mattia Sappa <mattiasappa@gmail.com>
2015-01-14 10:25:01 +0100

    gestione utente

--- app/src/main/java/com/borcellippa/iconquilini/HomeActivity.java
+++ app/src/main/java/com/borcellippa/iconquilini/HomeActivity.java
@@ -5,17 +5,33 @@ package com.borcellippa.iconquilini;
  */
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.borcellippa.resources.utente.Utente;
+
 /**
  *
  * Home Screen Activity
  */
 public class HomeActivity extends Activity {
 
+    Utente u;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Displays Home Screen
         setContentView(R.layout.home);
+
+        u = (Utente)getIntent().getSerializableExtra("utente");
+
+       // TextView t = (TextView)findViewById(R.id.testoProva);
+        //t.setText(u.toString());
+
+
+
     }
 
 }
--- app/src/main/java/com/borcellippa/iconquilini/LoginActivity.java
+++ app/src/main/java/com/borcellippa/iconquilini/LoginActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,9 +26,7 @@ import com.loopj.android.http.RequestParams;
 import java.io.UnsupportedEncodingException;
 
 /**
- *
- * Login Activity Class 
- *
+ * Login Activity Class
  */
 public class LoginActivity extends Activity {
 
@@ -45,20 +45,44 @@ public class LoginActivity extends Activity {
     EditText ipET;
 
 
+    // autocompletamento email
+    private static final String[] EMAIL = new String[]{
+            "mober@hotmail.it", "mattiasappa@gmail.com", "ciccicicci99@libero.it"
+    };
+
+    // autocompletamento indiritto ip
+    private static final String[] IP = new String[]{
+            "10.17.2.242", "10.17.2.254"
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+    // In the onCreate method
         Log.d(TAG, "Application Start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
         // Find Error Msg Text View control by ID
-        errorMsg = (TextView)findViewById(R.id.login_error);
+        errorMsg = (TextView) findViewById(R.id.login_error);
+
+
         // Find Email Edit View control by ID
-        emailET = (EditText)findViewById(R.id.loginEmail);
+        emailET = (EditText) findViewById(R.id.loginEmail);
+        // richiamo la textview su cui attivo l'autocompletamento per settare le impostazioni
+        AutoCompleteTextView email = (AutoCompleteTextView) findViewById(R.id.loginEmail);
+        ArrayAdapter<String> emailAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, EMAIL);
+        email.setAdapter(emailAdapter);
+
         // Find Password Edit View control by ID
-        pwdET = (EditText)findViewById(R.id.loginPassword);
+        pwdET = (EditText) findViewById(R.id.loginPassword);
 
-        ipET = (EditText)findViewById(R.id.loginIp);
-
+        ipET = (EditText) findViewById(R.id.loginIp);
+        AutoCompleteTextView ip = (AutoCompleteTextView) findViewById(R.id.loginIp);
+        ArrayAdapter<String> ipAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, IP);
+        ip.setAdapter(ipAdapter);
 
 
         // Instantiate Progress Dialog object
@@ -74,7 +98,7 @@ public class LoginActivity extends Activity {
      *
      * @param view
      */
-    public void loginUser(View view){
+    public void loginUser(View view) {
         Log.d(TAG, "Test email e password");
         // Get Email Edit View Value
         String email = emailET.getText().toString();
@@ -83,18 +107,16 @@ public class LoginActivity extends Activity {
 
         String ip = ipET.getText().toString();
 
-
         Log.d(TAG, email);
         Log.d(TAG, password);
         Log.d(TAG, ip);
 
-
         // Instantiate Http Request Param Object
         RequestParams params = new RequestParams();
         // When Email Edit View and Password Edit View have values other than Null
-        if(Utility.isNotNull(email) && Utility.isNotNull(password)){
+        if (Utility.isNotNull(email) && Utility.isNotNull(password)) {
             // When Email entered is Valid
-            if(Utility.validate(email)){
+            if (Utility.validate(email)) {
                 // Put Http parameter username with value of Email Edit View control
                 params.put("email", email);
                 // Put Http parameter password with value of Password Edit Value control
@@ -104,12 +126,12 @@ public class LoginActivity extends Activity {
                 invokeWS(params, ip);
             }
             // When Email is invalid
-            else{
+            else {
                 Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
             }
         }
         // When any of the Edit View control left blank
-        else{
+        else {
             Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
         }
 
@@ -120,20 +142,19 @@ public class LoginActivity extends Activity {
      *
      * @param params
      */
-    public void invokeWS(RequestParams params, String ip){
+    public void invokeWS(RequestParams params, String ip) {
         // Show Progress Dialog
         prgDialog.show();
         // Make RESTful webservice call using AsyncHttpClient object
         AsyncHttpClient client = new AsyncHttpClient();
-        Log.d(TAG,"invokeWS");
+        Log.d(TAG, "invokeWS");
         //client.get("http://"+ ip + ":8080/I_coinquilini-war/webresources/Utente/testGet",params ,new AsyncHttpResponseHandler() {
-        client.post("http://"+ ip + ":8080/I_coinquilini-war/webresources/Utente/validaLogin",params ,new AsyncHttpResponseHandler() {
+        client.post("http://" + ip + ":8080/I_coinquilini-war/webresources/Utente/validaLogin", params, new AsyncHttpResponseHandler() {
 
-                // When the response returned by REST has Http response code '200'
-
+            // When the response returned by REST has Http response code '200'
             @Override
             public void onSuccess(int i, Header[] headers, byte[] byteResponse) {
-                Log.d(TAG,"onSuccess");
+                Log.d(TAG, "onSuccess");
 
                 String response = null;
                 try {
@@ -146,24 +167,29 @@ public class LoginActivity extends Activity {
                 // Hide Progress Dialog
                 prgDialog.hide();
                 try {
-
-
-                    // errore da qua in giù....
-
                     Gson g = new Gson();
                     Utente u = g.fromJson(response, Utente.class);
 
                     System.out.println("UTENTE: " + u.getCognome());
                     Log.d(TAG, u.getCognome());
 
+                    /*
+                    *   Gestire errore login
+                    *
+                    * */
 
-                    if(u != null) {
+
+                    if (u != null) {
                         Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
                         // Navigate to Home screen
-                        navigatetoHomeActivity();
+                        if (u.getCasa() != null) {
+                            navigatetoHomeActivity(u);
+                        } else {
+                            navigatetoHomeErrorActivity(u);
+                        }
                     }
                     // Else display error message
-                    else{
+                    else {
                         errorMsg.setText("Credenziali errate");
                         Toast.makeText(getApplicationContext(), "Credenziali errate", Toast.LENGTH_LONG).show();
                     }
@@ -178,34 +204,40 @@ public class LoginActivity extends Activity {
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
 
-                Log.d(TAG,"onFailure");
+                Log.d(TAG, "onFailure");
 
                 // Hide Progress Dialog
                 prgDialog.hide();
                 // When Http response code is '404'
-                if(statusCode == 404){
+                if (statusCode == 404) {
                     Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
                 }
                 // When Http response code is '500'
-                else if(statusCode == 500){
+                else if (statusCode == 500) {
                     Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
                 }
                 // When Http response code other than 404, 500
-                else{
+                else {
                     Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
                 }
-
-
             }
         });
+    }
+
+
+    private void navigatetoHomeErrorActivity(Utente utente) {
+
     }
 
     /**
      * Method which navigates from Login Activity to Home Activity
      */
-    public void navigatetoHomeActivity(){
-        Intent homeIntent = new Intent(getApplicationContext(),HomeActivity.class);
+    public void navigatetoHomeActivity(Utente utente) {
+        Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
         homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        homeIntent.putExtra("utente", utente);
+
         startActivity(homeIntent);
     }
 
